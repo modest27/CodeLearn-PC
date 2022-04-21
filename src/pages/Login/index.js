dd7f1c42bf428bf1e86bb04660a102961bf2b40a
modest_ <1342994266@qq.com>
2022-04-21 19:31:32 +0800

    '登录功能-提示与loading优化'

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
-import { Card, Form, Input, Button, Checkbox } from 'antd'
+import { Card, Form, Input, Button, Checkbox, message } from 'antd'
 import './index.scss'
 import logo from 'assets/logo.png'
 import { login } from 'api/user'
 export default class Login extends Component {
+  state = {
+    // 加载状态
+    loading: false
+  }
   render() {
     return (
       <div className="login">
@@ -17,7 +21,7 @@ export default class Login extends Component {
             initialValues={{
               mobile: '13911111111',
               code: '246810',
-              agree: true,
+              agree: true
             }}
           >
             <Form.Item
@@ -25,12 +29,12 @@ export default class Login extends Component {
               rules={[
                 {
                   required: true,
-                  message: '手机号不能为空',
+                  message: '手机号不能为空'
                 },
                 {
                   pattern: /^1[3-9]\d{9}$/,
-                  message: '手机号格式错误',
-                },
+                  message: '手机号格式错误'
+                }
               ]}
             >
               <Input placeholder="请输入你的手机号" autoComplete="off" />
@@ -41,12 +45,12 @@ export default class Login extends Component {
               rules={[
                 {
                   required: true,
-                  message: '验证码不能为空',
+                  message: '验证码不能为空'
                 },
                 {
                   pattern: /^\d{6}$/,
-                  message: '验证码格式错误',
-                },
+                  message: '验证码格式错误'
+                }
               ]}
             >
               <Input placeholder="请输入验证码" autoComplete="off"></Input>
@@ -67,18 +71,15 @@ export default class Login extends Component {
                   //   }
                   // },
                   // 自定义校验规则
-                  validator: (rule, value) =>
-                    value
-                      ? Promise.resolve()
-                      : Promise.reject(new Error('请阅读并同意用户协议')),
-                },
+                  validator: (rule, value) => (value ? Promise.resolve() : Promise.reject(new Error('请阅读并同意用户协议')))
+                }
               ]}
             >
-              <Checkbox>我已阅读并同意[隐私条款]和[用户协议]</Checkbox>
+              <Checkbox>我已阅读并同意【隐私条款】和【用户协议】</Checkbox>
             </Form.Item>
 
             <Form.Item>
-              <Button type="primary" htmlType="submit" block>
+              <Button type="primary" htmlType="submit" block loading={this.state.loading}>
                 登录
               </Button>
             </Form.Item>
@@ -89,19 +90,27 @@ export default class Login extends Component {
   }
 
   onFinish = async ({ mobile, code }) => {
+    this.setState({
+      loading: true
+    })
     try {
       const res = await login(mobile, code)
-      console.log(res)
       // 登录成功
-      // 1. 保存token
-      localStorage.setItem('token', res.data.token)
-      // 2. 跳转到首页
-      this.props.history.push('/home')
+
       // 3. 提示消息
-      alert('登录成功')
+      message.success('登录成功', 1, () => {
+        // 1. 保存token
+        localStorage.setItem('token', res.data.token)
+        // 2. 跳转到首页
+        this.props.history.push('/home')
+      })
     } catch (error) {
       // console.dir(error)
-      alert(error.response.data.message)
+      message.warning(error.response.data.message, 1, () => {
+        this.setState({
+          loading: false
+        })
+      })
     }
   }
 }
